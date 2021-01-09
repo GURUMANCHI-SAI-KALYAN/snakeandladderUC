9b93049cb12039b26c7435450a31ac0c7dd4d051
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2021-01-09 22:19:10 +0530

    ADDED:playerposition100UC4 program added

--- snakeandladder.java
+++ snakeandladder.java
@@ -1,24 +1,28 @@
 
 public class snakeandladder {
-
+	
 	public static void main(String[] args) {
-	int position = 10;
+	int position = 0;
 	int LADDER_GAME = 0;
         int SNAKE_GAME = 1;
         int NOCHANCE_GAME = 2;
 
-	int option_player = (int) Math.floor(Math.random() * 10) % 2;
-
 	int DIE_NUMBER = (int) Math.floor(Math.random() * 10) % 5 + 1;
-	if (option_player == LADDER_GAME)
+	while(position <= 100) {
+	 int option_player = (int) Math.floor(Math.random() * 10) % 3;
+
+	if (option_player == LADDER_GAME) 
 		position += DIE_NUMBER;
 
-	else if (option_player == SNAKE_GAME)
+	else if (option_player == SNAKE_GAME) 
 		  position -= DIE_NUMBER;
+
+			 if (position < 0) 
+				position = 0;
 
 	else if (option_player == NOCHANCE_GAME)
 		position = position;
-
 	System.out.println("player position:" + position);
-      }
+	}
+	}
 }
